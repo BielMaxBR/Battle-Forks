f32a76a7273110fd993963da89cbc27ec00b7b33
Gabriel Maximo <gbielmaximo@gmail.com>
2020-05-13 19:01:24 +0000

    saving...

--- index.js
+++ index.js
@@ -29,13 +29,19 @@ function preload ()
         { frameWidth: 32, frameHeight: 48 }
     );
 }
-var platforms;
-
+var platforms, stars;
+var score = 0;
+var scoreText;
+var bombs;
 function create ()
 {
     cursors = this.input.keyboard.createCursorKeys();
 
     this.add.image(400, 300, 'sky');
+
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+
 
     platforms = this.physics.add.staticGroup();
 
@@ -70,8 +76,28 @@ function create ()
         repeat: -1
     });
 
+    stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+    });
+
+
+    bombs = this.physics.add.group();
+
+    this.physics.add.collider(bombs, platforms);
+
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
     this.physics.add.collider(player, platforms);
-}
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+    }
 
 function update ()
 {
@@ -99,3 +125,38 @@ function update ()
         player.setVelocityY(-330);
     }
 }
+
+function hitBomb(player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+}
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    }
+}   
