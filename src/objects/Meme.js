1adaec103895a739a2e84b26da1b82613b89827b
Gabriel Maximo <gbielmaximo@gmail.com>
2021-07-03 19:59:09 +0000

    overlap funcionando

--- src/objects/Meme.js
+++ src/objects/Meme.js
@@ -1,35 +1,38 @@
-import Container from './Container.js'
+// import Container from './Container.js'
 
 export default class Meme extends Phaser.GameObjects.Sprite {
     constructor(scene, name, { texture, frames, animsConfig }) {
         super(scene, 0, 0, texture, frames)
-        
+
         scene.physics.add.existing(this, false)
         scene.add.existing(this)
-
+        
         this.body.offset.y = 32
         this.body.height = 32
         
         this.name = name
-        this.children = new Container(scene, 0, 0)
-
+        // this.children = new Container(scene, 0, 0)
+        
         // criar o sprite e suas animações
-        // criar os eventos de colisão
         this.#createAnimations(texture, animsConfig)
+        // criar os eventos de colisão
+        scene.physics.add.overlap(this, scene.teams.p2, this.overlapCallback, null, scene)
         // criar o gerenciador de animações
         this.play('walk')
         // criar os eventos de ataque
         // criar o sistema de morte e delete
-        console.log(this.body.touching)
     }
     update() {
         this.body.setVelocity(0);
-        if (this.x <= 500) {
+        if (this.x < 500) {
             this.body.setVelocityX(100)
         } else if (this.anims.currentAnim.key != 'idle') {
             this.play('idle')
         }
 
+    }
+    overlapCallback(thisMeme, outro) {
+        console.log('aha')
     }
     #createAnimations(texture, { frameRate, anims }) {
         for (const anim of anims) {
--- src/scenes/Battle.js
+++ src/scenes/Battle.js
@@ -11,16 +11,28 @@ export default class Battle extends Phaser.Scene {
             startFrame: 0,
             endFrame: 17,
         })
+        this.createTeams()
     }
     create() {
-        console.log(this)
+
         this.meme = new Square(this)
         this.meme.x = 64
         this.meme.y = 64
+
+        this.algo = this.add.rectangle(500, 64, 64, 64, 0xffffff)
+        this.teams.p2.add(this.algo)
+        this.physics.add.existing(this.algo, true)
+        this.algo.x = 500
+        this.algo.y = 64
         
         console.log('batalha iniciada')
     }
     update() {
         this.meme.update()
+    }
+    createTeams() {
+        this.teams = {}
+        this.teams.p1 = this.physics.add.group()
+        this.teams.p2 = this.physics.add.group()
     }
 }
